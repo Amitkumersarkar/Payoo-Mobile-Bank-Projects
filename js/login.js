a1032723248b33145f72a4e618e57c5fc76ad472
Amitkumersarkar <amitekumersarkar@gmail.com>
2024-12-01 15:14:56 +0600

    added updated code

--- js/login.js
+++ js/login.js
@@ -3,8 +3,18 @@ document.getElementById('btn-login').addEventListener('change', function (event)
     console.log('login button clicked');
     // prevent default behavior
     event.preventDefault();
-
+    // pin number
+    const pinNumber = document.getElementById('pin-number').value;
     // get the phone number
     const phoneNumber = document.getElementById('phone-number').value;
-    console.log(phoneNumber);
+    console.log(phoneNumber, pinNumber);
+
+    // this is temporary. 
+    if (phoneNumber === '5' && pinNumber === '12345') {
+        console.log('you are logged in');
+        window.location.href = '/home.html';
+    }
+    else {
+        alert('wrong phone number or pin');
+    }
 })
