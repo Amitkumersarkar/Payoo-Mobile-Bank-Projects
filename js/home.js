234f0e99ccdcef136ca9fbf7d12f468d44d2fdd0
Amitkumersarkar <amitekumersarkar@gmail.com>
2024-12-01 20:10:31 +0600

    added ui dom

--- js/home.js
+++ js/home.js
@@ -9,5 +9,26 @@ document.getElementById('btn-add-money').addEventListener('click', function (eve
     // get the pin number provided
     const pinNumberInput = document.getElementById('pin-number').value;
     console.log(pinNumberInput);
+
+    // verify pin number
+    if (pinNumberInput === '1234') {
+        console.log('adding money to your account')
+        // get the current balance
+        const balance = document.getElementById('account-balance').innerText;
+        console.log(typeof balance);
+
+        // add addMoneyInput with balance
+        const addMoneyNumber = parseFloat(addMoneyInput);
+        const balanceNumber = parseFloat(balance);
+        const newBalance = addMoneyNumber + balanceNumber;
+        console.log(newBalance);
+
+        // updated balance in the ui dom
+        document.getElementById('account-balance').innerText;
+
+    }
+    else {
+        alert('Failed to add money! Please Try Again')
+    }
 });
 
