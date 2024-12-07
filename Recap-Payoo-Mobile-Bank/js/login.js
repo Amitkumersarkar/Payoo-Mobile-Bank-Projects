ffe0764983cd0ac5712c60436a6376a6f54b5602
Amitkumersarkar <amitekumersarkar@gmail.com>
2024-12-08 03:01:50 +0600

    added changing loaction

--- Recap-Payoo-Mobile-Bank/js/login.js
+++ Recap-Payoo-Mobile-Bank/js/login.js
@@ -1,9 +1,18 @@
 // form submit reloading the page
 document.getElementById('login-btn').addEventListener('click', function (event) {
     event.preventDefault();
-    console.log('login btn clicked');
-
     // get the phone number
     const phoneNumber = document.getElementById('phone-number').value;
-    console.log(phoneNumber);
-})
+
+    const pinNumber = document.getElementById('pin-number').value;
+    console.log(phoneNumber, pinNumber);
+    // this is temporary .we don't we this method
+    if (phoneNumber === '5' && pinNumber === '1234') {
+        console.log('Your Log In');
+        window.location.href = '/home.html';
+    }
+    else {
+        alert('Wrong Phone Number Or Pin Number');
+    }
+
+});
