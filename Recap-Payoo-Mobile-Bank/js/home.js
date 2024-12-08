0e4215ec563eee4882ec530b3fbc13969ab2bd3c
Amitkumersarkar <amitekumersarkar@gmail.com>
2024-12-08 19:22:41 +0600

    added event balance section

--- Recap-Payoo-Mobile-Bank/js/home.js
+++ Recap-Payoo-Mobile-Bank/js/home.js
@@ -9,4 +9,24 @@ document.getElementById('btn-add-money').addEventListener('click', function (eve
     const pinNumber = document.getElementById('pin-number').value;
 
     console.log(addMoney, pinNumber);
+
+    // verify pin number
+    if (pinNumber === '1234') {
+        console.log('adding money to your account');
+
+        // get the current balance
+        const balance = document.getElementById('account-balance').innerText;
+        console.log(balance);
+
+        // add  addMoney with account balance
+        const addMoneyNumber = parseFloat(addMoney);
+        const balanceNumber = parseFloat(balance);
+        const newBalance = addMoneyNumber + balanceNumber;
+        console.log(newBalance);
+        // updated balance in ui and show total balance
+        document.getElementById('account-balance').innerText = newBalance;
+    }
+    else {
+        alert('Failed to adding money in your account ...! please try again');
+    }
 });
